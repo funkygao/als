b4f5fd07758d17e84e54d1fc9ee33dcbce3efed2
funkygao <funky.gao@gmail.com>
2013-12-27 15:42:11 +0800

    msg time struct

--- message.go
+++ message.go
@@ -2,34 +2,40 @@ package als
 
 import (
 	json "github.com/bitly/go-simplejson"
+	"time"
 )
 
-type Message struct {
-	Area    string
-	Ts      uint64 // timestamp in UTC
-	Payload string // raw msg content, should be json string
+type AlsMessage struct {
+	Area     string
+	Ts       uint64 // timestamp in UTC
+	Payload  string // raw msg content, should be json string
+	Priority int    // set by app
 }
 
-// Convert text line to Message
-func NewMessage(line string) (*Message, error) {
+// Convert text line to AlsMessage
+func NewAlsMessage(line string, priority int) (*AlsMessage, error) {
 	area, ts, payload, err := ParseAlsLine(line)
 	if err != nil {
 		return nil, err
 	}
 
-	msg := &Message{Area: area, Ts: ts, Payload: payload}
-	return msg, nil
+	return &AlsMessage{Area: area, Ts: ts, Payload: payload, Priority: priority}, nil
 }
 
-func (this *Message) PayloadJson() (*json.Json, error) {
+func (this *AlsMessage) PayloadJson() (*json.Json, error) {
 	return MsgToJson(this.Payload)
 }
 
-func (this *Message) MarshalPayload() ([]byte, error) {
+func (this *AlsMessage) MarshalPayload() ([]byte, error) {
 	js, err := this.PayloadJson()
 	if err != nil {
 		return nil, err
 	}
 
 	return js.MarshalJSON()
+}
+
+// Convert timestamp from uint64 to struct
+func (this *AlsMessage) Time() time.Time {
+	return time.Unix(int64(this.Ts), 0)
 }
