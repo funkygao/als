9931b8fd338bdeed4458c7b03fea8e7c68a512f5
funkygao <funky.gao@gmail.com>
2014-01-07 12:35:24 +0800

    mv

--- als_test.go
+++ als_test.go
@@ -66,19 +66,6 @@ func TestGroupInt(t *testing.T) {
 	assert.Equal(t, "", GroupInt(0, ranges))
 }
 
-func TestCardinalityCounter(t *testing.T) {
-	c := NewCardinalityCounter()
-	c.Add("dau", 34343434)
-	c.Add("dau", 45454)
-	c.Add("dau", 888)
-	assert.Equal(t, uint64(3), c.Count("dau"))
-
-	c.Reset("msg")
-	c.Add("msg", "we are in China")
-	c.Add("msg", "where are you")
-	assert.Equal(t, uint64(2), c.Count("msg"))
-}
-
 func TestGetAttr(t *testing.T) {
 	type foo struct {
 		Name string
