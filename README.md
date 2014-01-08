a1e96715ed1bf843f3246cca217348e46b253579
funkygao <funky.gao@gmail.com>
2014-01-08 21:53:06 +0800

    line unit test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 als
 ===
 
-application logging system
+application logging system helper library
 
 [![Build Status](https://travis-ci.org/funkygao/als.png?branch=master)](https://travis-ci.org/funkygao/als)
---
+++ line_test.go
@@ -0,0 +1,33 @@
+package als
+
+import (
+	"github.com/funkygao/assert"
+	"testing"
+)
+
+func TestParseValidAlsLine(t *testing.T) {
+	line := `us,1387789257065,{"uri":"\/?fb_source=canvas_bookmark","_log_info":{"uid":8664420,"snsid":"100005855171890","level":29,"gender":"female","ab":"a","payment_cash":197,"script_id":2324196651,"serial":1,"uri":"\/","host":"172.31.1.244","ip":"209.202.60.244","callee":"POST+\/+24c55bb0","sid":null, "logfile": "/var/log/a.log"}}`
+	area, ts, _, err := parseAlsLine(line)
+	assert.Equal(t, "us", area)
+	assert.Equal(t, uint(1387789257065/1000), ts)
+	assert.Equal(t, nil, err)
+}
+
+func TestParseInvalidAlsLine(t *testing.T) {
+	var line string
+	line = `,1387789257065,foo`
+	_, _, _, err := parseAlsLine(line)
+	assert.Equal(t, ErrEmptyArea, err)
+
+	line = ``
+	_, _, _, err = parseAlsLine(line)
+	assert.Equal(t, ErrEmptyLine, err)
+
+	line = `us,blah,bar`
+	_, _, _, err = parseAlsLine(line)
+	assert.Equal(t, ErrTimestampInvalid, err)
+
+	line = `us,1387789257065`
+	_, _, _, err = parseAlsLine(line)
+	assert.Equal(t, ErrFieldNotEnough, err)
+}
