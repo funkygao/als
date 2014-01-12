e2ccd2947ebbf0dda6e62daf10785656ed0bb1fe
funkygao <funky.gao@gmail.com>
2014-01-12 10:58:19 +0800

    auto judge key type

--- message.go
+++ message.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	json "github.com/funkygao/go-simplejson"
 	"path/filepath"
+	"reflect"
 	"strings"
 	"time"
 )
@@ -141,6 +142,25 @@ func (this *AlsMessage) ValueOfKey(keyName string) (val interface{}, err error) 
 
 	val = this.payloadJson.DeepGet(keyName)
 	return
+}
+
+func (this *AlsMessage) NormalizedValueOfKey(keyName string) (val interface{}, err error) {
+	val, err = this.ValueOfKey(keyName)
+	if err != nil {
+		return
+	}
+
+	v := reflect.ValueOf(val)
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return int64(v.Uint())
+	case reflect.Float32, reflect.Float64:
+		return v.Float()
+	}
+
+	return v, nil
 }
 
 // Payload field value by key name and key type
