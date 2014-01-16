bd8b2b99be89929fde6608dce9ce518d96a00a11
funkygao <funky.gao@gmail.com>
2014-01-16 11:03:49 +0800

    auto correction of timestamp

--- line.go
+++ line.go
@@ -1,8 +1,14 @@
 package als
 
 import (
+	"math"
 	"strconv"
 	"strings"
+	"time"
+)
+
+var (
+	baseUTC = uint64(time.Now().UTC().Unix())
 )
 
 func parseAlsLine(line string) (area string, ts uint64, msg string, err error) {
@@ -35,8 +41,12 @@ func parseAlsLine(line string) (area string, ts uint64, msg string, err error) {
 	if ts > 1283931748344 {
 		ts /= 1000
 	} else if ts < 1262275200 {
+		// 2010-01-01 00:00:00 +0800 CST
 		err = ErrTimestampInvalid
 		return
+	} else if math.Abs(float64(ts-baseUTC)) > 21600 {
+		// 6 hours gap? should correct it
+		ts = uint64(time.Now().UTC().Unix())
 	}
 
 	msg = fields[2]
