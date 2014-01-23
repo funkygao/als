650086ccf10394553aeffa7f2a4f40ade49ddc0d
funkygao <funky.gao@gmail.com>
2014-01-23 19:07:53 +0800

    more benchmark

--- bench_test.go
+++ bench_test.go
@@ -1,6 +1,8 @@
 package als
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -8,41 +10,18 @@ const (
 	jsonLineForTest = `us,1389326456474,{"uri":"\/?fb_source=notification&request_ids=629862167081523%2C231759870340420%2C597190080352387%2C640624999328961%2C235464713291862%2C753053901389297%2C790469374302126%2C192819610918125%2C1409213372656992%2C1395677210684824%2C219547141565670%2C445351695593355%2C353291448144469%2C374894915987858%2C1405041129742942%2C1386152901642951%2C1444273795788958%2C268848269934670&ref=notif&app_request_type=user_to_user&notif_t=app_request","_log_info":{"uid":10304512,"snsid":"100006490632784","level":39,"gender":"male","ab":{"pay":"a","quest":"a"},"payment_cash":13,"script_id":9524283412,"serial":1,"uri":"\/","host":"172.31.7.194","ip":"81.65.52.251","callee":"POST+\/+44eae87","sid":null,"elapsed":0.014667987823486}}`
 )
 
-func BenchmarkLogfileExt(b *testing.B) {
+func BenchmarkParseUint(b *testing.B) {
+	// parseAlsLine will use this func
+	t := "1389326456474"
 	for i := 0; i < b.N; i++ {
-		l := NewAlsLogfile()
-		l.SetPath("/var//funplus/logs/fp_rstory/history/session_mm_20131208230103_1")
-		l.Ext()
-		l.SetPath("/var/bi_first_payment.10.log")
-		l.Ext()
+		strconv.ParseUint(t, 10, 64)
 	}
 }
 
-func BenchmarkMessageClone(b *testing.B) {
-	msg := NewAlsMessage()
-	if err := msg.FromLine(jsonLineForTest); err != nil {
-		panic(err)
-	}
-
-	var marshaled []byte
+func BenchmarkSplitN(b *testing.B) {
+	// parseAlsLine will use this func
 	for i := 0; i < b.N; i++ {
-		nm := msg.Clone()
-		b.StopTimer()
-		b.Logf("%#v", nm)
-		marshaled, _ = nm.MarshalPayload()
-		b.Logf("%v", string(marshaled))
-		b.StartTimer()
-	}
-	b.SetBytes(int64(len([]byte(jsonLineForTest))))
-}
-
-func BenchmarkCamelCaseName(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		l := NewAlsLogfile()
-		l.SetPath("/var//funplus/logs/fp_rstory/history/session_mm_20131208230103_1")
-		l.CamelCaseName()
-		l.SetPath("/var/bi_first_payment.10.log")
-		l.CamelCaseName()
+		strings.SplitN(jsonLineForTest, ",", 3)
 	}
 }
 
@@ -52,6 +31,22 @@ func BenchmarkParseAlsLine(b *testing.B) {
 		parseAlsLine(jsonLineForTest)
 	}
 	b.SetBytes(int64(len([]byte(jsonLineForTest))))
+}
+
+func BenchmarkLogfileExt(b *testing.B) {
+	l := NewAlsLogfile()
+	for i := 0; i < b.N; i++ {
+		l.SetPath("/var//funplus/logs/fp_rstory/history/session_mm_20131208230103_1")
+		l.Ext()
+	}
+}
+
+func BenchmarkCamelCaseName(b *testing.B) {
+	l := NewAlsLogfile()
+	for i := 0; i < b.N; i++ {
+		l.SetPath("/var//funplus/logs/fp_rstory/history/session_mm_20131208230103_1")
+		l.CamelCaseName()
+	}
 }
 
 func BenchmarkAlsMessageMarshalPayload(b *testing.B) {
@@ -96,4 +91,22 @@ func BenchmarkAlsMessageSetField(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		msg.SetField("foo", "bar")
 	}
+}
+
+func BenchmarkMessageClone(b *testing.B) {
+	msg := NewAlsMessage()
+	if err := msg.FromLine(jsonLineForTest); err != nil {
+		panic(err)
+	}
+
+	var marshaled []byte
+	for i := 0; i < b.N; i++ {
+		nm := msg.Clone()
+		b.StopTimer()
+		b.Logf("%#v", nm)
+		marshaled, _ = nm.MarshalPayload()
+		b.Logf("%v", string(marshaled))
+		b.StartTimer()
+	}
+	b.SetBytes(int64(len([]byte(jsonLineForTest))))
 }
